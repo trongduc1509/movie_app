1c2868c612c58f1f734cfd2755556db07e9d97b0
trongduc1509 <trongduc1509@gmail.com>
2022-09-05 14:11:26 +0700

    refactor home_page

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,9 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
